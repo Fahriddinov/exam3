586c8a008a1b0de61defeadd12ab17b7ceffe2bd
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2024-08-08 18:37:36 +0500

    initial commit

--- main.js
+++ main.js
@@ -1,69 +1,69 @@
 //Birinchi savol
-// let a = 5;
-//  let b = 2;
-//  let c = a % b;
-//  let arr = [c];
-//  console.log(arr)
+let a = 5;
+ let b = 2;
+ let c = a % b;
+ let arr = [c];
+ console.log(arr)
 
 //Ikkinchi savol
-// const RandomNumber = Math.round(Math.random() * 10);
-// alert(`Random son: ${RandomNumber}`);
+const RandomNumber = Math.round(Math.random() * 10);
+alert(`Random son: ${RandomNumber}`);
 
 //Uchinchi savol
-// const num = 12.510;
-// console.log(Math.floor(num));
+const num = 12.510;
+console.log(Math.floor(num));
 
 //Tortinchi savol
-// let word = "MARS IT SCHOOL";
-// function findLenght(param) {
-//     alert(word.length);
-// }
-// findLenght(word)
+let word = "MARS IT SCHOOL";
+function findLenght(param) {
+    alert(word.length);
+}
+findLenght(word)
 
 //Beshinchi savol
-// function printWord() {
-//     for (let i = 0; i < 10; i++) {
-//         console.log("MARS IT SCHOOL");
-//     }
-// }
-// printWord();
+function printWord() {
+    for (let i = 0; i < 10; i++) {
+        console.log("MARS IT SCHOOL");
+    }
+}
+printWord();
 
 // Oltinchi savol
-// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z"];
+let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z"];
 
-// let name = "muhammadamin";
+let name = "muhammadamin";
 
-// let ismYegdi = name.split('').every(harf => harflar.includes(harf));
+let ismYegdi = name.split('').every(harf => harflar.includes(harf));
 
-// if (ismYegdi) {
-//   console.log(name);
-// } else {
-//   console.log("Ismingizdagi ba'zi harflar mavjud emas.");
-// }
+if (ismYegdi) {
+  console.log(name);
+} else {
+  console.log("Ismingizdagi ba'zi harflar mavjud emas.");
+}
 
 //  Yettinchi savol
-// const age =prompt(`Yoshingizni kiriting:`)
+const age =prompt(`Yoshingizni kiriting:`)
 
-// if (age == 18) {
-//     alert("Siz 18 yoshdasiz")
-// } else if (age > 18) {
-//     alert("Siz 18 yoshdan kattasiz")
-// } else {
-//     alert("Siz 18 yoshda emas")
-// }
+if (age == 18) {
+    alert("Siz 18 yoshdasiz")
+} else if (age > 18) {
+    alert("Siz 18 yoshdan kattasiz")
+} else {
+    alert("Siz 18 yoshda emas")
+}
 
 //  Sakkizinchi savol
-// let str = "Muhammadmin"
-// let str2 = str.split("").reverse().join("");
-// console.log(str2)
+let str = "Muhammadmin"
+let str2 = str.split("").reverse().join("");
+console.log(str2)
 
 //  Oninchi savol
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// let even_numbers = [];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let even_numbers = [];
 
-// for (let number of numbers) {
-//     if (number % 2 === 0) {
-//         even_numbers.push(number);
-//     }
-// }
-// console.log(even_numbers);
+for (let number of numbers) {
+    if (number % 2 === 0) {
+        even_numbers.push(number);
+    }
+}
+console.log(even_numbers);
